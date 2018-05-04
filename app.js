67b85a8bdfccb62f1a8717101425687f3504ec81
Elvin Samuel <elvinsamuel1@gmail.com>
2018-05-04 07:22:06 +0000

    Continued bot project, but need an emulator to test. Don't have access to one in c9 as far as I know. Might test on another computer.

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+var restify = require('restify');
 var builder = require('botbuilder');
 
-var connector = new builder.ConsoleConnector().listen();
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send("You said: %s", session.message.text);
+// Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function(){
+    console.log('%s listening to %s', server.name, server.url);
 });
+
+// Create chat connector to communicate with Bot framework
+var connector = new builder.ChatConnector({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Listen for messages
+server.post('/api/messages', connector.listen());
+
+// Receive messages from user and respond
+var bot = new builder.UniversalBot(connector, function(session){
+    session.send("You said: %s", session.message.text);
+})
